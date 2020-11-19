2c9b7d43229715090fb1e46648d68927f48c26e2
chethanjs97 <chethanjschethu@gmail.com>
2020-11-19 17:22:35 +0530

    Added : validating First Name

--- src/com/UserRegistration/UserRegistration.java
+++ src/com/UserRegistration/UserRegistration.java
@@ -1,8 +1,29 @@
 package com.UserRegistration;
 
+import java.util.Scanner;
+
 public class UserRegistration {
-	public static void main(String[] args) {
-		System.out.println("Welcome to User Registration Program");
+	
+	Scanner sc = new Scanner(System.in);
+
+	public void ValidateFirstName() {
+		String expression = "^[A-Z][a-zA-Z]{2,}$";
+
+		System.out.println("Enter the First Name Starting With Capital Letter");
+		String firstName = sc.next();
+
+		if (firstName.matches(expression))
+			System.out.println("Validation Success");
+		else {
+			System.out.println("Invalid First Name Try Again With Valid Name");
+			ValidateFirstName();
+		}
 	}
 
+	public static void main(String[] args) {
+		System.out.println("Welcome to User Registration");
+
+		UserRegistration user = new UserRegistration();
+		user.ValidateFirstName();
+	}
 }
